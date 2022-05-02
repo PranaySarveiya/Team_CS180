eeafc01cd6583e83fcb949d30ea20c7e0700a9e0
PranaySarveiya <pranaysarveiya@gmail.com>
2022-05-01 22:44:56 -0700

    Refactoring and Cleaning
    Python logging
    Function Python docstrings
    try except cases
    consistent variable naming

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	path('search/', views.search, name='search'),
 	path('welcome/', views.welcome, name='welcome'),
-	path('index/', views.index, name='index'),
+	#path('index/', views.index, name='index'),
 	#path('search-state/', views.AccidentByState, name = 'search-state'),	#eventually probably want to make this it's own html page
 	path('top-5-states/', views.Top5States, name='top-5-states'),
 	path('modify/', views.Modify, name='modify')
