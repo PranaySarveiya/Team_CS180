5fd66bf8b44beb7bba0b166ca5eeb12da1331638
Chandler Mahkorn <ChandlerMahkorn@gmail.com>
2022-05-01 22:53:01 -0700

    Changed graph plot to by month instead of by day

--- mysite/hello/graph_plot.py
+++ mysite/hello/graph_plot.py
@@ -17,34 +17,41 @@ if __name__ == '__main__':
 
 	print("Building date count. This will take a while...")
 	# Iterate through all dates between 2016 to the start of 2022
-	while(currentDate.year != 2022):
-		count = 0
-
-		# Show at least some progress
-		if (currentDate.year != year):
-			year += 1
-			print(currentDate.year)
-
-		# Iterate through the entire list... for every date
-		for crash in accidents.list:
-			date = datetime.strptime(crash.start_time.split(" ")[0], dateformat)
-
-			if (date == currentDate):
-				count += 1
-
-		dates.append([currentDate, count])
-		currentDate += timedelta(days = 1)
-
-	# Split the list into explicitly dates or count
+	dates = [0] * 72
+	mult = 0
 	x = []
-	y = []
-	for item in dates:
-		x.append(item[0])
-		y.append(item[1])
+
+	for row in accidents.list:
+		date = datetime.strptime(row.start_time.split(" ")[0], dateformat)
+
+		if (date.year == 2017):
+			mult = 1
+		elif (date.year == 2018):
+			mult = 2
+		elif (date.year == 2019):
+			mult = 3
+		elif (date.year == 2020):
+			mult = 4
+		elif (date.year == 2021):
+			mult = 5
+
+		if (date.year == 2016):
+				dates[date.month - 1] += 1
+		else:
+				dates[(date.month - 1) + (12 * mult)] += 1
+
+	x = []
+	year = 2016
+	for i in range(1, 73):
+		if (i % 12 == 0):
+			x.append(str(year) + "-12-1")
+			year += 1
+		else:
+			x.append(str(year) + "-" + str(i % 12) + "-1")
 
 	# Display the data as a graph
-	plt.plot(x, y)
-	plt.xlabel('Date of crash')
-	plt.ylabel('Amount of crashes')
-	plt.title('Car Crashes 2016 - 2021')
+	plt.plot(x, dates)
+	plt.xlabel('Month of crash')
+	plt.ylabel('Amount of crashes per month')
+	plt.title('Car Crashes per month 2016 - 2021')
 	plt.show()
